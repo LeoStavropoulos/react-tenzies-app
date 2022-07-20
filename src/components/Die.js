021328884c0fc20da9c1877cf435f8aa1c675bf8
Leonardos <stavropoulos.leonardos@gmail.com>
2022-07-21 02:11:17 +0300

    Added conditional styling for held dice

--- src/components/Die.js
+++ src/components/Die.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Die(props) {
     return (
-        <div className="die-face">
+        <div className="die-face" style={props.isHeld ? {backgroundColor:"#59E391"} : {backgroundColor: "#fff"}} onClick={props.holdDice}>
             <h2 className={"die-num"}>{props.value}</h2>
         </div>
     )
