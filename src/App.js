3a8407abcfbecfd7155400f3022b059045de8da6
Leonardos <stavropoulos.leonardos@gmail.com>
2022-07-21 02:11:54 +0300

    Added hold dice functionality

--- src/App.js
+++ src/App.js
@@ -7,23 +7,28 @@ export default function App() {
     function allNewDice() {
         const newDice = []
         for (let i = 0; i < 10; i++) {
-            newDice.push({value: Math.floor(Math.random() * 6) + 1, isHeld: false})
+            newDice.push({value: Math.floor(Math.random() * 6) + 1, isHeld: false, id: i + 1})
         }
         return newDice
     }
 
-    function handleClick() {
-        setDice(allNewDice());
+    function rollDice() {
+        setDice(oldDice => oldDice.map(die => die.isHeld ? die : {...die, value: Math.floor(Math.random() * 6) + 1}));
+        console.log(dice)
     }
 
-    const diceElements = dice.map((die, index) => <Die key={index} value={die.value}/>)
+    function holdDice(id) {
+        setDice(oldDice => oldDice.map(die => die.id === id ? {...die, isHeld: !die.isHeld} : die))
+    }
+
+    const diceElements = dice.map((die, index) => <Die key={index} value={die.value} isHeld={die.isHeld} holdDice={() => holdDice(die.id)}/>)
 
     return(
         <main>
             <div className="dice-container">
                 {diceElements}
             </div>
-            <button className={"roll-dice"} onClick={handleClick}>Roll </button>
+            <button className={"roll-dice"} onClick={rollDice}>Roll </button>
         </main>
     )
 }
